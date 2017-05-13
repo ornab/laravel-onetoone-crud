71793d99eb7bb68577fcac323596348aa24d8b19
ornab <hzamil17@gmail.com>
2017-05-13 22:35:40 +0600

    updated

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,33 @@
 |
 */
 
+use App\User;
+use App\Address;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/insert', function(){
+    
+    $user = User::findOrFail(1);
+    
+    $address = new Address(['name' => 'Hospital Road, Maijdee']);
+    
+    
+    $user->address()->save($address);
+});
+
+
+Route::get('/update', function(){
+    
+   //$address = Address::where('user_id', 1)->get(); 
+    
+   $address = Address::whereUserId(1)->first(); 
+    
+    $address->name = "Updated, Noakhali";
+    
+    $address->save();
+    
 });
